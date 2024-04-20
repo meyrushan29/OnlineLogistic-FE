c2c085e72e8cc2018e33cda96c2c5172f9ded6a5
laxshi24 <laxshisiva@gmail.com>
2024-04-20 17:53:32 +0530

    SupplierFinalChangeFrontEnd

--- frontend/olms/src/Components/SupplierManage/UpdateSupplier.js
+++ frontend/olms/src/Components/SupplierManage/UpdateSupplier.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-import {  FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+
 
 function UpdateSupplier() {
   const { id } = useParams();
@@ -13,21 +14,11 @@ function UpdateSupplier() {
   const [OrderID, setOrderID] = useState('');
   const [Country, setCountry] = useState('');
   const [Category, setCategory] = useState('');
+  const [Status, setStatus] = useState('');
 
   const navigate = useNavigate();
 
-  // Function to generate a random ID
-  const generateRandomID = () => {
-    return Math.random().toString(36).substr(2, 9);
-  };
-
-  // Generate Supplier ID and Order ID when component mounts
-  useEffect(() => {
-    const newSupplierID = `SID${generateRandomID()}`;
-    const newOrderID = `OID${generateRandomID()}`;
-    setSupplierID(newSupplierID);
-    setOrderID(newOrderID);
-  }, []);
+  
 
   useEffect(() => {
     axios.get(`http://localhost:3001/getSupplier/${id}`)
@@ -39,6 +30,9 @@ function UpdateSupplier() {
         setCompanyName(result.data.CompanyName);
         setCountry(result.data.Country);
         setCategory(result.data.Category);
+        setSupplierID(result.data.SupplierID);
+        setOrderID(result.data.OrderID);
+        setStatus(result.data.Status)
       })
       .catch(err => console.log(err));
   }, [id]);
@@ -62,7 +56,7 @@ function UpdateSupplier() {
     e.preventDefault();
 
     // Check if all required fields are filled
-    if (!Name || !Email || !PhoneNumber || !CompanyName || !Country) {
+    if (!Name || !Email || !PhoneNumber || !CompanyName || !Country || !Status || !SupplierID || !OrderID ) {
       alert('Please fill in all required fields');
       return;
     }
@@ -96,7 +90,7 @@ function UpdateSupplier() {
       return;
     }
 
-    axios.put(`http://localhost:3001/UpdateSupplier/${id}`, { Name, Email, PhoneNumber, CompanyName, Country, Category })
+    axios.put(`http://localhost:3001/UpdateSupplier/${id}`, { Name, Email, PhoneNumber, CompanyName, Country, Category ,SupplierID ,OrderID,Status })
       .then(result => {
         console.log(result);
         navigate('../supplier');
@@ -105,74 +99,40 @@ function UpdateSupplier() {
   };
 
   return (
-    <div className='flex h-75 justify-center items-center mt-4'>
-      <div className='w-50 bg-gray-100 rounded p-3'>
+    <div className="flex items-center justify-center min-h-screen  bg-gray-100">
+      <div className=' h-screen  w-75  ml-96 mr-10 '>
+      <div className="bg-white shadow-md rounded px-2 pt-2 pb-2 w-full max-w-md mt-2">
         <form onSubmit={handleSubmit}>
-          <h2 className='col-md-6 pl-14'>Edit Supplier Details</h2>
-          <div className='mb-2'>
-            <TextField
-              id="name"
-              label="Name"
-              placeholder="Enter Name"
-              fullWidth
-              value={Name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
+          <h2 className='col-md-10 pl-0 mb-3'>Edit Supplier Details</h2>
+          <div className='mb-1'>
+            <TextField id="name" label="Name" placeholder="Enter Name" fullWidth value={Name} onChange={(e) => setName(e.target.value)} required />
           </div>
-          <div className='mb-2'>
-            <TextField
-              id="id"
-              label="Supplier ID"
-              placeholder="Enter ID"
-              fullWidth
-              value={SupplierID}
-              InputProps={{ readOnly: true }}
-            />
+          <div className="mb-1">
+            <TextField id="ID" label="Supplier ID" placeholder="Enter Supplier ID" fullWidth value={SupplierID} onChange={(e) => setSupplierID(e.target.value)}
+              required/>
           </div>
-          <div className='mb-2'>
-            <TextField
-              id="phoneNumber"
-              label="Phone Number"
-              placeholder="Enter Phone number"
-              fullWidth
-              value={PhoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              required
-            />
+           
+          <div className='mb-1'>
+            <TextField id="phoneNumber" label="Phone Number" placeholder="Enter Phone number" fullWidth value={PhoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}
+               required />
           </div>
-          <div className='mb-2'>
-            <TextField
-              id="email"
-              label="Email"
-              placeholder="Enter Email"
-              fullWidth
-              value={Email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+          <div className='mb-1'>
+            <TextField id="email" label="Email" placeholder="Enter Email" fullWidth value={Email} onChange={(e) => setEmail(e.target.value)}
+              required />
           </div>
-          <div className='mb-2'>
-            <TextField
-              id="companyName"
-              label="Company Name"
-              placeholder="Enter Company Name"
-              fullWidth
-              value={CompanyName}
-              onChange={(e) => setCompanyName(e.target.value)}
-              required
-            />
+          <div className='mb-1'>
+            <TextField id="companyName" label="Company Name" placeholder="Enter Company Name" fullWidth value={CompanyName} onChange={(e) => setCompanyName(e.target.value)}
+              required />
           </div>
-          <div className="mb-4">
+          <div className="mb-1">
+            <TextField id="OID" label="Order ID" placeholder="Enter Order ID" fullWidth value={OrderID} onChange={(e) => setOrderID(e.target.value)}
+              required/>
+          </div>
+          <div className="mb-1">
             <FormControl fullWidth>
               <InputLabel id="category-label">Supplier Category</InputLabel>
-              <Select
-                labelId="category-label"
-                id="category"
-                value={Category}
-                onChange={(e) => setCategory(e.target.value)}
-                required
-              >
+              <Select labelId="category-label" id="category" value={Category} onChange={(e) => setCategory(e.target.value)}
+                required >
                 <MenuItem value="">Select Category</MenuItem>
                 <MenuItem value="Clothing and Apparel">Clothing and Apparel</MenuItem>
                 <MenuItem value="Home and Garden">Home and Garden</MenuItem>
@@ -184,32 +144,26 @@ function UpdateSupplier() {
               </Select>
             </FormControl>
           </div>
-          <div className='mb-2'>
-            <TextField
-              id="orderId"
-              label="Order ID"
-              placeholder="Enter Order ID"
-              fullWidth
-              value={OrderID}
-              InputProps={{ readOnly: true }}
-            />
+          <div className="mb-1">
+            <FormControl fullWidth>
+              <InputLabel id="Status-label">Status</InputLabel>
+              <Select labelId="Status-label" id="Status" value={Status} onChange={(e) => setStatus(e.target.value)} required >
+                <MenuItem value="">Select Status</MenuItem>
+                <MenuItem value="Active">Active</MenuItem>
+                <MenuItem value="Inactive">Inactive</MenuItem>
+                </Select>
+            </FormControl>
           </div>
-          <div className='mb-2'>
-            <TextField
-              id="Country"
-              label="Country"
-              placeholder="Enter Country"
-              fullWidth
-              value={Country}
-              onChange={(e) => setCountry(e.target.value)}
-              required
-            />
+          <div className='mb-1'>
+            <TextField id="Country" label="Country" placeholder="Enter Country" fullWidth value={Country} onChange={(e) => setCountry(e.target.value)}
+              required />
           </div>
           <button type="submit" className="bg-cyan-800 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded mr-2">
             Update
         </button>
 
         </form>
+      </div>
       </div>
     </div>
   );
--- frontend/olms/src/Components/SupplierManage/supplier.js
+++ frontend/olms/src/Components/SupplierManage/supplier.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { Button, TextField } from '@mui/material';
 import axios from 'axios';
 import { Table } from 'react-bootstrap';
+import { Checkbox } from '@mui/material';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
 
 const Supplier = () => {
   const [suppliers, setSuppliers] = useState([]);
   const [searchName, setSearchName] = useState('');
   const [searchOID, setSearchOID] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedStatus, setSelectedStatus] = useState('');
   const [categories, setCategories] = useState([]);
+  const [selectedSuppliers, setSelectedSuppliers] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3001/sup')
@@ -20,22 +24,36 @@ const Supplier = () => {
       .catch(err => console.log(err));
   }, []);
 
-  const handleDelete = (id) => {
-    const confirmDelete = window.confirm("Are you sure you want to delete this supplier?");
-    if (confirmDelete) {
-      axios.delete(`http://localhost:3001/Deletesupplier/${id}`)
-        .then(res => {
-          console.log(res);
-          setSuppliers(suppliers.filter(supplier => supplier._id !== id));
+  const handleCheckboxChange = (supplierId) => {
+    if (selectedSuppliers.includes(supplierId)) {
+      setSelectedSuppliers(selectedSuppliers.filter(id => id !== supplierId));
+    } else {
+      setSelectedSuppliers([...selectedSuppliers, supplierId]);
+    }
+  };
+
+  const handleDeleteSelected = () => {
+    if (window.confirm('Are you sure you want to delete the selected suppliers?')) {
+      axios.delete('http://localhost:3001/Deletesupplier/', { data: { ids: selectedSuppliers } })
+        .then(result => {
+          console.log(result);
+          // Refresh supplier list after deletion
+          axios.get('http://localhost:3001/sup')
+            .then(result => {
+              setSuppliers(result.data);
+              setSelectedSuppliers([]);
+            })
+            .catch(err => console.log(err));
         })
         .catch(err => console.log(err));
     }
   };
 
   const filteredSuppliers = suppliers.filter(supplier =>
-    supplier.Name.toLowerCase().includes(searchName.toLowerCase()) &&
-    supplier.OrderID.toLowerCase().includes(searchOID.toLowerCase()) &&
-    (selectedCategory ? supplier.Category === selectedCategory : true)
+    (supplier.Name?.toLowerCase().includes(searchName.toLowerCase()) || false) &&
+    (supplier.OrderID?.toLowerCase().includes(searchOID.toLowerCase()) || false) &&
+    (selectedCategory ? supplier.Category === selectedCategory : true) &&
+    (selectedStatus ? supplier.Status === selectedStatus : true)
   );
 
   const handleAddSupplier = () => {
@@ -52,78 +70,128 @@ const Supplier = () => {
     }
   };
 
+  const generatePDF = () => {
+    const doc = new jsPDF();
+    doc.autoTable({
+      head: [['Name', 'Supplier ID', 'Phone Number', 'Email', 'Company Name', 'Order ID', 'Country', 'Supplier Category', 'Status']],
+      body: filteredSuppliers.map(supplier => [supplier.Name, supplier.SupplierID, supplier.PhoneNumber, supplier.Email, supplier.CompanyName, supplier.OrderID, supplier.Country, supplier.Category, supplier.Status]),
+    });
+    doc.save('supplier_list.pdf');
+  };
+
   return (
-    <div className="container-fluid w-full mt-2 mb-2 flex justify-end">
-      <div className="border rounded p-2">
-        <div className="flex justify-between mb-2 h-10">
-          <h1 className="text-3xl font-bold">Supplier List</h1>
-          <Button onClick={handleAddSupplier} variant="contained" color="primary">Add Supplier+</Button>
+    <div className="container px-4 py-8 mt-2">
+      <div className="bg-white border-8 rounded-lg shadow mt-2 p-6">
+        <h1 className="text-3xl font-bold mb-4">Supplier List</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+          <div>
+            <input
+              type="text"
+              placeholder="Search supplier name"
+              value={searchName}
+              onChange={(e) => setSearchName(e.target.value)}
+              className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none w-full shadow-sm"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Search OID"
+              value={searchOID}
+              onChange={(e) => setSearchOID(e.target.value)}
+              className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none w-full shadow-sm"
+            />
+          </div>
         </div>
-        <div className="grid grid-cols-2 gap-4 mb-2 mt-2">
-          <TextField
-            type="text"
-            id="name"
-            placeholder="Search supplier name"
-            value={searchName}
-            onChange={(e) => setSearchName(e.target.value)}
-          />
-          <TextField
-            type="text"
-            id="OID"
-            placeholder="Search OID"
-            value={searchOID}
-            onChange={(e) => setSearchOID(e.target.value)}
-          />
-          </div>
-          <div className='w-50 mb-3 mt-3'>
-            <select
-              value={selectedCategory}
-              onChange={(e) => setSelectedCategory(e.target.value)}
-              className="form-select mt-1"
-            >
-              <option value="">All Categories</option>
-              {categories.map((category, index) => (
-                <option key={index} value={category}>{category}</option>
-              ))}
-            </select>
-          </div>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Supplier ID</th>
-              <th>Phone Number</th>
-              <th>Email</th>
-              <th>Company Name</th>
-              <th>Order ID</th>
-              <th>Country</th>
-              <th>Category</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredSuppliers.map((supplier, index) => (
-              <tr key={index}>
-                <td>{supplier.Name}</td>
-                <td>{supplier.SupplierID}</td>
-                <td>{supplier.PhoneNumber}</td>
-                <td>{supplier.Email}</td>
-                <td>{supplier.CompanyName}</td>
-                <td>{supplier.OrderID}</td>
-                <td>{supplier.Country}</td>
-                <td>{supplier.Category}</td>
-                <td>
-                  <button onClick={() => handleEditSupplier(supplier._id)} className="bg-red-900 text-white font-bold py-1 px-2 rounded">
-                    Edit
-                  </button>
-                  <button onClick={() => handleDelete(supplier._id)} className="bg-cyan-900 text-white font-bold py-1 ml-1 mt-2 px-2 rounded">
-                    Delete
-                  </button>
-                </td>
-              </tr>
+        <div className="grid grid-cols-1 md:grid-cols-4 mb-4">
+          <select
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+            className="border border-gray-300 rounded-lg w-80 mr-6  h-15 px-4 py-2 focus:outline-none"
+          >
+            <option value="">All Categories</option>
+            {categories.map((category, index) => (
+              <option key={index} value={category}>{category}</option>
             ))}
-          </tbody>
-        </Table>
+          </select>
+
+          <select
+            value={selectedStatus}
+            onChange={(e) => setSelectedStatus(e.target.value)}
+            className="border border-gray-300 rounded-lg ml-8 w-80 h-15 px-4 py-2 focus:outline-none"
+          >
+            <option value="">All Statuses</option>
+            <option value="Active">Active</option>
+            <option value="Inactive">Inactive</option>
+          </select>
+          <div className='w-48 h-10 mt-6 ml-72'>
+            <button
+              onClick={handleDeleteSelected}
+              className={`bg-pink-700 hover:bg-blue-900 text-white font-bold  py-2 px-4 rounded-lg ${selectedSuppliers.length === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+              disabled={selectedSuppliers.length === 0}
+            >
+              Delete Selected
+            </button>
+          </div>
+          <div className="w-48 h-10 mt-6 items-end ml-40 mr-6">
+            <button
+              onClick={handleAddSupplier}
+              className="bg-red-900 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-lg"
+            >
+              Add Supplier
+            </button>
+          </div>
+          <div className="w-48 h-10 mt-6 items-end ml-40 mr-6">
+            <button
+              onClick={generatePDF}
+              className="bg-green-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-lg"
+            >
+              Generate PDF
+            </button>
+          </div>
+        </div>
+        <div className="overflow-x-auto">
+          <Table striped bordered hover className='border-2'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Supplier ID</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+                <th>Company Name</th>
+                <th>Order ID</th>
+                <th>Country</th>
+                <th>Supplier Category</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredSuppliers.map((supplier, index) => (
+                <tr key={index}>
+                  <td>{supplier.Name}</td>
+                  <td>{supplier.SupplierID}</td>
+                  <td>{supplier.PhoneNumber}</td>
+                  <td>{supplier.Email}</td>
+                  <td>{supplier.CompanyName}</td>
+                  <td>{supplier.OrderID}</td>
+                  <td>{supplier.Country}</td>
+                  <td>{supplier.Category}</td>
+                  <td>{supplier.Status}</td>
+                  <td><button onClick={() => handleEditSupplier(supplier._id)} className="bg-cyan-900 text-white font-bold py-1 px-2 rounded">
+                    Edit</button>
+                  </td>
+                  <td>
+                    <Checkbox
+                      checked={selectedSuppliers.includes(supplier._id)}
+                      onChange={() => handleCheckboxChange(supplier._id)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
       </div>
     </div>
   );
